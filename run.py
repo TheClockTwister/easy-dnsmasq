fb6224256bac259b82e6adca434ba34e0a3f2059
leonwiesen <wiesen.leon@gmail.com>
2020-01-11 17:53:05 +0100

    Added ENV for refresh interval

--- run.py
+++ run.py
@@ -2,10 +2,16 @@
 import time
 import os
 
-server = os.getenv("dns_server")
-os.system("echo server={} > /etc/dnsmasq.conf".format(server))
+# Get variables from Dockerfile or user arguments
+server = os.getenv("DNS_SERVER")
+refresh = os.getenv("REFRESH_INTERVAL") # in minutes
 
+# Set dnsmasq configs appropriately
+os.system("echo server={} > /etc/dnsmasq.conf".format(server)) # dnsmasq.conf flushed, only forward server
+
+
+# "Etrypoint" for container loop
 while True:
-   os.system("service dnsmasq restart")
-   print("reload")
-   time.sleep(60*60) #restart every hour
+    print("{} - Restarting dnsmasq...".format(time.strftime("%d.%m.%Y %H:%M:%S")))
+    os.system("service dnsmasq restart")
+    time.sleep(60*refresh)
